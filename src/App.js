fefbf5ed8220a11d9bd23315bd6ddc0f942adca0
nguyen son <z0zb0yz0z@gamil.com>
2021-11-18 13:39:07 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-
-import React, { Suspense ,lazy} from 'react'
-import Routers from './router'
+import React, { Suspense, lazy } from "react";
+import Routers from "./router";
 // const Routers = lazy(() => import('./router'))
-import "./style/main.css"
-import "./style/abc.css"
+import "./style/main.css";
+import "./style/abc.css";
 function App() {
   return (
     <div className="App">
       <Routers>
-        {/* <Suspense fallback={<div> xin chào các bạn </div>}>
-            xinc ahfo 
-        </Suspense> */}
+        <Suspense fallback={<div> xin chào các bạn </div>}>xinc ahfo</Suspense>
       </Routers>
-
-
     </div>
   );
 }
